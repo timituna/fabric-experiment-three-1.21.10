0ec065b730cde3f4846c15a8fc7a4eda6d0830f8
Tuna Mert İyiarı <tunamert04@gmail.com>
2025-11-21 23:08:53 +0300

    Made suspicious substance comsumable and added status effects

--- src/main/java/net/tuna/experiment_three/items/ModItems.java
+++ src/main/java/net/tuna/experiment_three/items/ModItems.java
@@ -7,10 +7,18 @@ import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.effect.MobEffect;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.component.Consumable;
+import net.minecraft.world.item.component.Consumables;
+import net.minecraft.world.item.consume_effects.ApplyStatusEffectsConsumeEffect;
 import net.tuna.experiment_three.ExperimentThree;
 
+import java.util.List;
 import java.util.function.Function;
 
 public class ModItems {
@@ -22,7 +30,18 @@ public class ModItems {
         });
     };
 
-    public static final Item SUSPICIOUS_SUBSTANCE = registerItem("suspicious_substance", Item::new, new Item.Properties());
+    private static List<MobEffectInstance> suspicious_substance_effects = List.of(new MobEffectInstance(MobEffects.POISON, 10 * 20, 1),
+            new MobEffectInstance(MobEffects.BLINDNESS, 5 * 20));
+
+    public static final Consumable POISON_FOOD_CONSUMABLE_COMPONENT = Consumables.defaultFood()
+            // The duration is in ticks, 20 ticks = 1 second
+            .onConsume(new ApplyStatusEffectsConsumeEffect(suspicious_substance_effects))
+            .build();
+    public static final FoodProperties POISON_FOOD_COMPONENT = new FoodProperties.Builder()
+            .alwaysEdible()
+            .build();
+    public static final Item SUSPICIOUS_SUBSTANCE = registerItem("suspicious_substance", Item::new,
+            new Item.Properties().food(POISON_FOOD_COMPONENT, POISON_FOOD_CONSUMABLE_COMPONENT));
 
     public static Item registerItem(
             String name,
